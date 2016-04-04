bda986f84fe5f1b2c5d87134eea619835b2cccef
salmedina <salbed.medina@gmail.com>
2016-04-04 16:11:06 -0400

    Updated query pipeline script with step 6 display results idea

--- VideoProcessing/query_pipeline.sh
+++ VideoProcessing/query_pipeline.sh
@@ -5,15 +5,20 @@ DB_FEATS_DIR = ''
 QUERY_FEATS_DIR = ''
 QUERY_SCORES = ''
 QUERY_RESULTS = ''
+TOP_N = 100
 
 # 1. Extract the keyframes from all the videos in video dir
 
 # 1.1. Preprocess the keyframes?
 
 # 2. Obtain the NetVLAD feature vectors
+matlab /r "getFeatVects($KEYFRAMES_DIR, $QUERY_FEATS_DIR)"
 
-# 3. Calculate the similarity scores for each key-frame
+# 3. Calculate the similarity scores (Top 100) for each key-frame
+python 
 
 # 4. Rank the video according to each key-frame score
 
 # 5. Save results
+
+# 6. Display results at full corpuse level and per video based on keyframes
