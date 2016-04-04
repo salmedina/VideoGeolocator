db447bcc5c8cceaa32441866436d4491373c9fef
salmedina <salbed.medina@gmail.com>
2016-04-04 02:41:04 -0400

    Add query pipeline structure script

---
+++ VideoProcessing/query_pipeline.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+VIDEOS_DIR = ''
+KEYFRAMES_DIR = ''
+DB_FEATS_DIR = ''
+QUERY_FEATS_DIR = ''
+QUERY_SCORES = ''
+QUERY_RESULTS = ''
+
+# 1. Extract the keyframes from all the videos in video dir
+
+# 1.1. Preprocess the keyframes?
+
+# 2. Obtain the NetVLAD feature vectors
+
+# 3. Calculate the similarity scores for each key-frame
+
+# 4. Rank the video according to each key-frame score
+
+# 5. Save results
